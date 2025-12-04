84845ba49b6a47523b4e070813ae10ad98265d75
javierForf <javierforf@gmail.com>
2025-12-04 09:59:53 -0600

    Update database connection configuration

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -5,14 +5,15 @@ dotenv.config();
 
 // Crear pool de conexiones
 const db = mysql.createPool({
-  host: process.env.DB_HOST || "mysql-bso80c84g4gcw4skc0cccws0.internal",
+  host: process.env.DB_HOST || "mysql-database-hsoso0s0ckwwcskso40ck0go.internal",
   user: process.env.DB_USER || "mysql",
-  password: process.env.DB_PASSWORD || "raFQ6d1AQDbQPcQSEHU4ew6y76QUJfj6gLaG8tTIh26KWy7hqGXf1zz7fh7f6ZBC",
-  database: process.env.DB_NAME || "default",
+  password: process.env.DB_PASSWORD || "UserPassword123!",
+  database: process.env.DB_NAME || "lifters", // <-- cambia si usaste otro nombre
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
-  charset: 'utf8mb4' 
+  charset: 'utf8mb4'
+});
 
 });
 
